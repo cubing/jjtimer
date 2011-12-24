17be58669438c053a7b778a0fe90f20e174739af
Joey Gouly <cardologist@gmail.com>
2011-12-24 22:50:24 +0000

    big rewrite of the generic scrambler

--- scrambler.js
+++ scrambler.js
@@ -1,13 +1,14 @@
 function rndEl(x){return x[Math.floor(Math.random()*x.length)];}
 
-	function isArray(obj){
-		return (obj instanceof Array);
- 		if(typeof obj=='object'){
-  		var test = obj.constructor.toString().match(/array/i); 
+function isArray(obj){
+	return (obj instanceof Array);
+	if(typeof obj=='object'){
+  	var test = obj.constructor.toString().match(/array/i); 
   		return (test != null);
-  		}
- 		return false;
-	}
+  }
+ 	return false;
+}
+
 var scrambler = {
 	scramblers: [],
 	current: undefined,	
@@ -21,41 +22,34 @@ var scrambler = {
 	},
 
 	generic: function(turns, suffixes, length) {
- return function(){
-var donemoves=[];
- var lastaxis;
- var i,j,k;
-  var s="";
-  lastaxis=-1;
-  for(j=0;j<length;j++){
-   var done=0;
-   do{
-    var first=Math.floor(Math.random()*turns.length);
-    var second=Math.floor(Math.random()*turns[first].length);
-    if(first!=lastaxis||donemoves[second]==0){
-     if(first==lastaxis){
-      donemoves[second]=1;
-      if(isArray(turns[first][second])){
-       s+=rndEl(turns[first][second])+rndEl(suffixes)+" ";
-      }else{
-       s+=turns[first][second]+rndEl(suffixes)+" ";
-      }
-     }else{
-      for(k=0;k<turns[first].length;k++){donemoves[k]=0;}
-      lastaxis=first;
-      donemoves[second]=1;
-      if(isArray(turns[first][second])){
-       s+=rndEl(turns[first][second])+rndEl(suffixes)+" ";
-      }else{
-       s+=turns[first][second]+rndEl(suffixes)+" ";
-      }
-     }
-     done=1;
-    }
-   }while(done==0);
-  }
-  return s;
-
-	};
-}
+		return function(){
+			var donemoves=[];
+			var lastaxis;
+			var i,j,k;
+			var s="";
+			lastaxis = -1;
+			for(j=0; j<length; j++){
+				var done = 0;
+				while(done == 0) {
+					var first = Math.floor(Math.random()*turns.length);
+					var second = Math.floor(Math.random()*turns[first].length);
+					if(first != lastaxis) {
+						for(k=0;k<turns[first].length;k++){donemoves[k]=0;}
+						lastaxis = first;
+					}
+					if(donemoves[second] == 0) {
+						donemoves[second] = 1;
+						if(isArray(turns[first][second])) {
+       				s += rndEl(turns[first][second])+rndEl(suffixes)+" ";
+      			}
+						else {
+							s+=turns[first][second]+rndEl(suffixes)+" ";
+						}
+						done = 1;
+					}
+   			}
+  		}
+  		return s;
+		};
+	}
 };
