22e7a8c1c63bc9195fecccec911bfa6e0523eeef
Joey Gouly <cardologist@gmail.com>
2011-12-25 02:17:27 +0000

    release.rb now has an option to build with closure compiler

--- release.rb
+++ release.rb
@@ -1,20 +1,22 @@
-first = false
-last = false
+js_files = %w(session timer scrambler ui)
+merge = js_files.map {|f| File.read f + ".js" }.join
+
+if ARGV[0] == "-o"
+	require "net/http"
+	require "uri"
+	
+	url = URI.parse "http://closure-compiler.appspot.com/compile"
+	http = Net::HTTP.new url.host, url.port
+	request = Net::HTTP::Post.new url.request_uri
+	request.set_form_data :compilation_level => 'ADVANCED_OPTIMIZATIONS', :output_format => 'text', :output_info => 'compiled_code', :js_code => merge 
+	response = http.request request
+	merge = response.body
+end
+
+output = File.read "jjtimer.html"
+output.gsub!(/<script type="text\/javascript" src="[^"]+"><\/script>\n/, "")
+output.gsub!(/<\/head>/, merge + "</head>")
+
 File.open("index.html", "w") do |out|
-	File.readlines("jjtimer.html").each do |l| 
-		if l =~ /<script type="text\/javascript" src="([^"]+)"><\/script>/
-			if !first
-				first = true
-				out.print "<script type=\"text/javascript\">\n"
-			end
-			last = true
-			out.print File.read $1
-		else
-			if last
-				out.print "\n</script>"
-			end
-			last = false
-			out.print l
-		end
-	end
+	out.print output
 end
