7b5b08476b8347ac26cef4b9cfb8f61cad9e7c73
Joey Gouly <cardologist@gmail.com>
2011-12-31 02:10:21 +0000

    Rewrite scrambler to use literal objects

--- mark2_333.js
+++ mark2_333.js
@@ -1,4 +1,17 @@
 load_external("http://www.cubing.net/mark2/inc/scramblers/scramble_333.js")
-scramble_manager.add(new scrambler("3x3-random-state", function() { scramblers["333"].initialize(null, Math);
-return scramblers["333"].getRandomScramble().scramble; }));
+
+var mark2_333 = {
+	name: '3x3-mk2',
+
+	scramble_func: function() {
+		var s = scramblers["333"].getRandomScramble(); 
+		return s.scramble;
+	},
+
+	selected: function(){
+		scramblers["333"].initialize(null, Math);
+	}
+};
+
+scramble_manager.add(mark2_333);
 ui.plugin_loaded("mark2_333");
