edf98e2952ae161d59a0cffa798e296087cf3229
joey <cardologist@gmail.com>
2012-01-16 23:54:58 +0000

    Add shortcut for 5x5

--- js/ui.js
+++ js/ui.js
@@ -403,6 +403,7 @@ var ui = (function() {
 
 		shortcuts.add_key_up('3'.charCodeAt(), {'shift': true, 'func': function() { scramble_manager.set($('scramble_menu').selectedIndex = 0); next_scramble(); }});
 		shortcuts.add_key_up('4'.charCodeAt(), {'shift': true, 'func': function() { scramble_manager.set($('scramble_menu').selectedIndex = 1); next_scramble(); }});
+		shortcuts.add_key_up('5'.charCodeAt(), {'shift': true, 'func': function() { scramble_manager.set($('scramble_menu').selectedIndex = 2); next_scramble(); }});
 		shortcuts.add_key_up('D'.charCodeAt(), {'shift': true, 'func': function(){ session.del(null); update_stats(); }});
 
 		if(localStorage)
